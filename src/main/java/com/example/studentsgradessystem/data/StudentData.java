5e8eaf0db5bf2790102abae98d5021569ddae596
daniel <danielane@qq.com>
2023-09-05 00:24:30 +0800

    Added pojo classes to data layer

--- src/main/java/com/example/studentsgradessystem/data/StudentData.java
+++ src/main/java/com/example/studentsgradessystem/data/StudentData.java
@@ -9,6 +9,36 @@ public class StudentData {
 
     long studentId;
     String studentName;
-    List<Grade> grades;
+    List<GradeData> grades;
 
+    public StudentData(long studentId, String studentName, List<GradeData> grades) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.grades = grades;
+    }
+
+
+    public long getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(long studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public List<GradeData> getGrades() {
+        return grades;
+    }
+
+    public void setGrades(List<GradeData> grades) {
+        this.grades = grades;
+    }
 }
