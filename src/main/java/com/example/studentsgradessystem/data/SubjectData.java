ae55826624a51ad2f1af458abf4aa462f0e421f4
daniel <danielane@qq.com>
2023-09-05 00:24:30 +0800

    Added pojo classes to data layer

--- src/main/java/com/example/studentsgradessystem/data/SubjectData.java
+++ src/main/java/com/example/studentsgradessystem/data/SubjectData.java
@@ -2,8 +2,13 @@ package com.example.studentsgradessystem.data;
 
 public class SubjectData {
 
+<<<<<<< HEAD
     private long subjectId;
     private String subjectName;
+=======
+    long subjectId;
+    String subjectName;
+>>>>>>> 73ee17f (Added pojo classes to data layer)
 
     public SubjectData(long subjectId, String subjectName) {
         this.subjectId = subjectId;
