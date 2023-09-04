9087aa19befa2f15c04e13225bb3532b775bd98e
daniel <danielane@qq.com>
2023-09-05 00:45:03 +0800

    Added StudentDB to the project

---
+++ src/main/java/com/example/studentsgradessystem/data/database/StudentDB.java
@@ -0,0 +1,9 @@
+package com.example.studentsgradessystem.data.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class StudentDB {
+
+
+}
