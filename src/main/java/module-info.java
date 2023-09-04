a3c9a6521076e40d1fed48e2d03676a1c051e4a4
daniel <danielane@qq.com>
2023-09-05 00:44:11 +0800

    Added database dependency to the project

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,12 @@
 module com.example.studentsgradessystem {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.studentsgradessystem to javafx.fxml;
     exports com.example.studentsgradessystem.data;
-    exports com.example.studentsgradessystem.domain.pojo;
-    exports com.example.studentsgradessystem.domain.react;
-    exports com.example.studentsgradessystem.domain.repositories;
+    exports com.example.studentsgradessystem.domain;
     exports com.example.studentsgradessystem.presentation;
     opens com.example.studentsgradessystem.presentation to javafx.fxml;
 }
