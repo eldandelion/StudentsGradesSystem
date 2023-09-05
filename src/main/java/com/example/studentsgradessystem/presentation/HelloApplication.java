56ec02342dd849cb3c39b98f69d6103df32928ac
daniel <danielane@qq.com>
2023-09-05 15:26:31 +0800

    tests

--- src/main/java/com/example/studentsgradessystem/presentation/HelloApplication.java
+++ src/main/java/com/example/studentsgradessystem/presentation/HelloApplication.java
@@ -1,16 +1,21 @@
 package com.example.studentsgradessystem.presentation;
 
 import com.example.studentsgradessystem.data.GradeData;
+import com.example.studentsgradessystem.data.StudentRepositoryImpl;
 import com.example.studentsgradessystem.data.SubjectData;
 import com.example.studentsgradessystem.data.database.GradeDB;
 import com.example.studentsgradessystem.data.database.StudentDB;
 import com.example.studentsgradessystem.data.database.SubjectDB;
+import com.example.studentsgradessystem.domain.pojo.Grade;
+import com.example.studentsgradessystem.domain.pojo.Student;
+import com.example.studentsgradessystem.domain.pojo.Subject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class HelloApplication extends Application {
@@ -23,15 +28,27 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
 
-        SubjectDB subjectDB = new SubjectDB();
+//        SubjectDB subjectDB = new SubjectDB();
 //
 //        subjectDB.insertSubject(new SubjectData(32, "math"));
 //        List<SubjectData> data = subjectDB.getSubjects();
 
-        SubjectData subjectData = new SubjectData(32, "math");
+//        SubjectData subjectData = new SubjectData(32, "math");
 
-        GradeDB gradeDB = new GradeDB(subjectDB);
+//        GradeDB gradeDB = new GradeDB(subjectDB);
 
+        StudentRepositoryImpl studentRepository = new StudentRepositoryImpl();
+        List<Grade> grades = new ArrayList<>();
+        grades.add(new Grade(new Subject(32, "math"), 21, 22));
+
+        studentRepository.insertNewStudent(new Student(31, "Max", grades));
+        List<Student> list = studentRepository.getAllStudents();
+
+
+
+        for (Student student : list) {
+            System.out.println(student.getStudentName());
+        }
 //        gradeDB.insertGrade(new GradeData(subjectData, 34243, 22));
 
 //        List<GradeData> data = gradeDB.getGradesByNumber(34243);
