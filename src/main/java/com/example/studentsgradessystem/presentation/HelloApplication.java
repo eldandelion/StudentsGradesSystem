dba87f8e505c9b903f055ba7d17eca890f1b7f2e
daniel <danielane@qq.com>
2023-09-05 14:04:15 +0800

    Created databases for all the data entities

--- src/main/java/com/example/studentsgradessystem/presentation/HelloApplication.java
+++ src/main/java/com/example/studentsgradessystem/presentation/HelloApplication.java
@@ -1,11 +1,17 @@
 package com.example.studentsgradessystem.presentation;
 
+import com.example.studentsgradessystem.data.GradeData;
+import com.example.studentsgradessystem.data.SubjectData;
+import com.example.studentsgradessystem.data.database.GradeDB;
+import com.example.studentsgradessystem.data.database.StudentDB;
+import com.example.studentsgradessystem.data.database.SubjectDB;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class HelloApplication extends Application {
     @Override
@@ -16,6 +22,23 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+
+        SubjectDB subjectDB = new SubjectDB();
+//
+//        subjectDB.insertSubject(new SubjectData(32, "math"));
+//        List<SubjectData> data = subjectDB.getSubjects();
+
+        SubjectData subjectData = new SubjectData(32, "math");
+
+        GradeDB gradeDB = new GradeDB(subjectDB);
+
+//        gradeDB.insertGrade(new GradeData(subjectData, 34243, 22));
+
+//        List<GradeData> data = gradeDB.getGradesByNumber(34243);
+//        for (GradeData gradeData : data) {
+//            System.out.println(gradeData.getStudentGrade());
+//            System.out.println(gradeData.getSubject().getSubjectName());
+//        }
         launch();
     }
 }
