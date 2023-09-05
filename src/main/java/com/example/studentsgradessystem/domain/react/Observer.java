ac43a90d3e2c91ac390333bee547cc205d4d17f7
daniel <danielane@qq.com>
2023-09-05 15:34:15 +0800

    observer's notify method gets list of data

--- src/main/java/com/example/studentsgradessystem/domain/react/Observer.java
+++ src/main/java/com/example/studentsgradessystem/domain/react/Observer.java
@@ -7,5 +7,5 @@ import java.util.List;
 //View implements observer, if data has been updated, repository,
 // which implements Publisher sends updated data to Observer
 public interface Observer {
-    void update(List<Student> students);
+    void update(List<Student> data);
 }
--- src/main/java/com/example/studentsgradessystem/domain/react/Publisher.java
+++ src/main/java/com/example/studentsgradessystem/domain/react/Publisher.java
@@ -1,5 +1,9 @@
 package com.example.studentsgradessystem.domain.react;
 
+import com.example.studentsgradessystem.domain.pojo.Student;
+
+import java.util.List;
+
 //Implemented by Repository
 //When data is updated in the SQL database,
 // Repository notifies UI with fresh data
@@ -7,6 +11,6 @@ public interface Publisher {
 
     void registerObserver(Observer observer);
     void unregisterObserver(Observer observer);
-    void notifyObservers();
+    void notifyObservers(List<Student> data);
 
 }
