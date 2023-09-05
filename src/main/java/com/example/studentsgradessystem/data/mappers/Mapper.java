8786c4cd7fbdb3deaa121fde4c5a52df260ef10f
daniel <danielane@qq.com>
2023-09-05 14:10:22 +0800

    started creating mapper

--- src/main/java/com/example/studentsgradessystem/data/mappers/Mapper.java
+++ src/main/java/com/example/studentsgradessystem/data/mappers/Mapper.java
@@ -1,7 +1,13 @@
 package com.example.studentsgradessystem.data.mappers;
 
+import com.example.studentsgradessystem.data.SubjectData;
+import com.example.studentsgradessystem.domain.pojo.Subject;
+
 public class Mapper {
 
 
-
+//    public static Subject subDataToEntity(SubjectData data) {
+//
+//        return new Subject()
+//    }
 }
