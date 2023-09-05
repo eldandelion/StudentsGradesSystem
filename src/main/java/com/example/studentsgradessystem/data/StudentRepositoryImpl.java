759460b32d90e8d70466971c62053ae9c1b566ac
daniel <danielane@qq.com>
2023-09-05 15:22:48 +0800

    implemented key repository methods

--- src/main/java/com/example/studentsgradessystem/data/StudentRepositoryImpl.java
+++ src/main/java/com/example/studentsgradessystem/data/StudentRepositoryImpl.java
@@ -1,6 +1,10 @@
 package com.example.studentsgradessystem.data;
 
 
+import com.example.studentsgradessystem.data.database.GradeDB;
+import com.example.studentsgradessystem.data.database.StudentDB;
+import com.example.studentsgradessystem.data.database.SubjectDB;
+import com.example.studentsgradessystem.data.mappers.Mapper;
 import com.example.studentsgradessystem.domain.repositories.StudentRepository;
 import com.example.studentsgradessystem.domain.pojo.Student;
 import com.example.studentsgradessystem.domain.react.Observer;
@@ -10,26 +14,39 @@ import java.util.List;
 public class StudentRepositoryImpl implements StudentRepository {
 
 
+    private StudentDB studentDB;
+    private GradeDB gradeDB;
+    private SubjectDB subjectDB;
+    public StudentRepositoryImpl() {
+        subjectDB = new SubjectDB();
+        gradeDB = new GradeDB(subjectDB);
+        studentDB = new StudentDB(gradeDB);
+    }
 
     @Override
-    public void getAllStudents() {
-
-
+    public List<Student> getAllStudents() {
+        return Mapper.listStuToEntity(studentDB.getAllStudents());
     }
 
     @Override
     public void insertNewStudent(Student student) {
-
+        if (studentDB.insertNewStudent(Mapper.entityToStudentData(student))) {
+            notifyObservers();
+        }
     }
 
     @Override
     public void updateStudent(Student student) {
-
+        if (studentDB.updateStudent(Mapper.entityToStudentData(student))) {
+            notifyObservers();
+        }
     }
 
     @Override
     public void deleteStudent(Student student) {
-
+        if (studentDB.deleteStudent(Mapper.entityToStudentData(student))) {
+            notifyObservers();
+        }
     }
 
     @Override
