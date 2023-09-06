ccf007f13bae7d3c70dd6489cce17b207861688d
daniel <danielane@qq.com>
2023-09-06 20:16:46 +0800

    added events for TeacherRepository.java

---
+++ src/main/java/com/example/studentsgradessystem/domain/events/TeacherObserver.java
@@ -0,0 +1,12 @@
+package com.example.studentsgradessystem.domain.events;
+
+import com.example.studentsgradessystem.domain.pojo.Student;
+import com.example.studentsgradessystem.domain.pojo.Teacher;
+
+import java.util.List;
+
+public interface TeacherObserver {
+    void update(List<Teacher> data);
+    void update(Teacher data);
+
+}
---
+++ src/main/java/com/example/studentsgradessystem/domain/repositories/TeacherRepository.java
@@ -0,0 +1,16 @@
+package com.example.studentsgradessystem.domain.repositories;
+
+import com.example.studentsgradessystem.domain.events.TeacherPublisher;
+import com.example.studentsgradessystem.domain.pojo.Student;
+import com.example.studentsgradessystem.domain.pojo.Teacher;
+
+public interface TeacherRepository extends TeacherPublisher {
+
+    void getAllTeachers();
+    void getTeacherByCredentials(String email, String password);
+    void insertNewTeacher(Teacher teacher);
+    void updateTeacher(Teacher teacher);
+    void deleteStudent(Teacher teacher);
+
+
+}
