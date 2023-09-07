7a9d05a52c9dc4bea93a96e8e96d8e4eb72d5835
daniel <danielane@qq.com>
2023-09-07 18:06:26 +0800

    added integration with the database

--- src/main/java/com/example/studentsgradessystem/presentation/LogInScreen.java
+++ src/main/java/com/example/studentsgradessystem/presentation/LogInScreen.java
@@ -1,20 +1,26 @@
 package com.example.studentsgradessystem.presentation;
 
+import com.example.studentsgradessystem.domain.Flags;
+import com.example.studentsgradessystem.domain.events.TeacherObserver;
+import com.example.studentsgradessystem.domain.pojo.Teacher;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class LogInScreen extends Application {
+
+
     private Stage primaryStage;
-    private static final String VALID_USERNAME = "admin"; // 预定义的有效用户名
-    private static final String VALID_PASSWORD = "password"; // 预定义的有效密码
-    private String registeredUsername = ""; // 用于存储已注册的用户名
-    private String registeredPassword = ""; // 用于存储已注册的密码
+
+    private final LogInViewModel logInViewModel = new LogInViewModel();
 
     public static void main(String[] args) {
         launch(args);
@@ -23,76 +29,98 @@ public class LogInScreen extends Application {
     @Override
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        primaryStage.setTitle("登录");
+        primaryStage.setTitle("成绩管理系统");
 
         // 创建登录界面
-        GridPane loginGrid = createLoginGrid();
 
-        Scene loginScene = new Scene(loginGrid, 300, 200);
+        Scene loginScene = new Scene(createLoginGrid(), 300, 250);
         primaryStage.setScene(loginScene);
         primaryStage.show();
     }
 
-    private GridPane createLoginGrid() {
-        GridPane grid = new GridPane();
-        grid.setAlignment(javafx.geometry.Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+    private VBox createLoginGrid() {
 
-        Label usernameLabel = new Label("学号:");
+        VBox verticalBox = new VBox(10);
+        verticalBox.setPadding(new Insets(25, 25, 25, 25));
+
+        Label usernameLabel = new Label("账号");
         TextField usernameField = new TextField();
-        usernameField.setPromptText("请输入学号");
-        Label passwordLabel = new Label("身份证后六位:");
+        usernameField.setPromptText("myemail@qq.com");
+        Label passwordLabel = new Label("密码");
         PasswordField passwordField = new PasswordField();
-        passwordField.setPromptText("请输入身份证后六位");
+        passwordField.setPromptText("**************");
         Button loginButton = new Button("登录");
-        Button registerButton = new Button("注册账号");
+        Label registerLabel = new Label("没有账号？注册");
 
-        HBox buttonsBox = new HBox(loginButton, registerButton);
-        buttonsBox.setSpacing(10);
+        registerLabel.setTextAlignment(TextAlignment.CENTER);
 
-        HBox.setHgrow(loginButton, Priority.ALWAYS);
-        HBox.setHgrow(registerButton, Priority.ALWAYS);
+        registerLabel.setStyle("-fx-background-color: white; -fx-text-fill: black;");
+
+        // Set event handlers for mouse hovering
+        registerLabel.setOnMouseEntered(event -> {
+            registerLabel.setStyle("-fx-background-color: white; -fx-text-fill: #4380e6;");
+        });
+
+        registerLabel.setOnMouseExited(event -> {
+            registerLabel.setStyle("-fx-background-color: white; -fx-text-fill: black;");
+        });
+
+
+        VBox buttonBox = new VBox(loginButton, registerLabel);
+        buttonBox.setAlignment(Pos.CENTER);
+        VBox.setMargin(loginButton, new Insets(20, 0, 12, 0));
         loginButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        registerButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
 
-        grid.add(usernameLabel, 0, 0);
-        grid.add(usernameField, 1, 0);
-        grid.add(passwordLabel, 0, 1);
-        grid.add(passwordField, 1, 1);
-        grid.add(buttonsBox, 1, 2);
-//        grid.add(registerButton, 1, 3);
+
+        verticalBox.getChildren().addAll(usernameLabel, usernameField, passwordLabel, passwordField, buttonBox);
 
         loginButton.setOnAction(e -> {
             String username = usernameField.getText();
             String password = passwordField.getText();
 
             // 在此处验证用户名和密码
-            if (validateLogin(username, password)) {
-//                 showMainUI();
-            } else {
-                showAlert("登录失败", "学号或身份证后六位不正确");
-            }
+            validateLogin(username, password);
+
         });
 
-        registerButton.setOnAction(e -> {
-            showRegistrationUI();
-        });
 
-        return grid;
+        registerLabel.setOnMouseClicked(mouseEvent -> showRegistrationUI());
+
+        return verticalBox;
     }
 
-    private boolean validateLogin(String username, String password) {
-        // 这里可以添加更强化的用户名和密码验证逻辑
-        // 例如，检查用户名和密码是否与已注册的帐户匹配
-        return username.equals(registeredUsername) && password.equals(registeredPassword);
+    private void validateLogin(String username, String password) {
+
+
+        logInViewModel.registerObserver(new TeacherObserver() {
+            @Override
+            public void update(List<Teacher> data) {
+
+            }
+            @Override
+            public void update(Teacher data) {
+                if (data != null) {
+
+                    System.out.println(data.getEmail());
+                    //save current session
+                    //show main ui
+//                    logInViewModel.unregisterObserver(this);
+                } else {
+                    showAlert("登录失败", "学号或身份证后六位不正确");
+                }
+            }
+
+            @Override
+            public void update(Flags flag) {
+
+            }
+        });
+        logInViewModel.getTeacherByCredentials(username, password);
     }
 
     private void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR);
         alert.setTitle(title);
-        alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
     }
@@ -104,23 +132,36 @@ public class LogInScreen extends Application {
 //        mainApp.start(mainStage);
 //    }
 
+    private static final String EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+
     private void showRegistrationUI() {
+
+
         // 在注册界面中创建帐户
         Dialog<String> registrationDialog = new Dialog<>();
-        registrationDialog.setTitle("注册");
-        registrationDialog.setHeaderText("请输入新的学号和身份证后六位:");
 
-        // 创建学号和身份证输入字段
+        VBox verticalBox = new VBox(10);
+        verticalBox.setPadding(new Insets(25, 25, 25, 25));
+
+        registrationDialog.setTitle("注册");
+
+
         TextField newUserField = new TextField();
+        TextField newUserEmail = new TextField();
         PasswordField newPasswordField = new PasswordField();
-        newUserField.setPromptText("请输入学号");
-        newPasswordField.setPromptText("请输入身份证后六位");
-        GridPane registrationGrid = new GridPane();
-        registrationGrid.add(new Label("新学号:"), 0, 0);
-        registrationGrid.add(newUserField, 1, 0);
-        registrationGrid.add(new Label("新密码:"), 0, 1);
-        registrationGrid.add(newPasswordField, 1, 1);
-        registrationDialog.getDialogPane().setContent(registrationGrid);
+        newUserField.setPromptText("蔡雅韵");
+        newUserEmail.setPromptText("myemail@qq.com");
+        newPasswordField.setPromptText("************");
+
+        verticalBox.getChildren().addAll(new Label("姓名"), newUserField, new Label("Email"), newUserEmail, new Label("新密码"), newPasswordField);
+        registrationDialog.getDialogPane().setContent(verticalBox);
+        registrationDialog.getDialogPane().setPrefHeight(250);
+        registrationDialog.getDialogPane().setPrefWidth(300);
 
         // 添加注册和取消按钮
         ButtonType registerButtonType = new ButtonType("注册", ButtonBar.ButtonData.OK_DONE);
@@ -128,13 +169,34 @@ public class LogInScreen extends Application {
 
         registrationDialog.setResultConverter(dialogButton -> {
             if (dialogButton == registerButtonType) {
-                String newUsername = newUserField.getText();
-                String newPassword = newPasswordField.getText();
+                String newUsername = newUserField.getText().trim();
+                String newPassword = newPasswordField.getText().trim();
+                String newEmail = newUserEmail.getText().trim();
 
-                // 在此处创建新的帐户
-                registeredUsername = newUsername;
-                registeredPassword = newPassword;
-                return "registered";
+                isValidEmail(newEmail);
+
+                logInViewModel.registerObserver(new TeacherObserver() {
+                    @Override
+                    public void update(List<Teacher> data) {
+
+                    }
+
+                    @Override
+                    public void update(Teacher data) {
+                        //save current session
+                        System.out.println(data.getEmail());
+                    }
+
+                    @Override
+                    public void update(Flags flag) {
+                        if (flag.equals(Flags.RegisterSuccess)) {
+                            System.out.println("Registered");
+                        } else {
+
+                        }
+                    }
+                });
+                logInViewModel.registerTeacher(newUsername, newEmail, newPassword);
             }
             return null;
         });
