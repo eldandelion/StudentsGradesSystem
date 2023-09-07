df6c7de04d909a2cefcfa8afa7e697ccabdeb5e1
daniel <danielane@qq.com>
2023-09-07 18:03:08 +0800

    added methods for flags

--- src/main/java/com/example/studentsgradessystem/domain/events/TeacherObserver.java
+++ src/main/java/com/example/studentsgradessystem/domain/events/TeacherObserver.java
@@ -1,5 +1,6 @@
 package com.example.studentsgradessystem.domain.events;
 
+import com.example.studentsgradessystem.domain.Flags;
 import com.example.studentsgradessystem.domain.pojo.Student;
 import com.example.studentsgradessystem.domain.pojo.Teacher;
 
@@ -8,5 +9,6 @@ import java.util.List;
 public interface TeacherObserver {
     void update(List<Teacher> data);
     void update(Teacher data);
+    void update(Flags flag);
 
 }
--- src/main/java/com/example/studentsgradessystem/domain/events/TeacherPublisher.java
+++ src/main/java/com/example/studentsgradessystem/domain/events/TeacherPublisher.java
@@ -1,5 +1,6 @@
 package com.example.studentsgradessystem.domain.events;
 
+import com.example.studentsgradessystem.domain.Flags;
 import com.example.studentsgradessystem.domain.pojo.Student;
 import com.example.studentsgradessystem.domain.pojo.Teacher;
 
@@ -8,8 +9,9 @@ import java.util.List;
 public interface TeacherPublisher {
 
 
-    void registerObserver(StudentObserver observer);
-    void unregisterObserver(StudentObserver observer);
+    void registerObserver(TeacherObserver observer);
+    void unregisterObserver(TeacherObserver observer);
     void notifyObservers(List<Teacher> data);
     void notifyObservers(Teacher data);
+    void notifyObservers(Flags flag);
 }
